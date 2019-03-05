4114e868f34a23f209adc6a232b00048f7f514a0
jiang501.com <406250613@qq.com>
2019-03-05 14:11:16 +0800

    mp0120

---
+++ mp0120/pages/details/details.js
@@ -0,0 +1,104 @@
+// pages/details/details.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: ['美国', '中国', '巴西', '日本'],
+    index: 0,
+    ifOutput: false,
+    ifShareRule: false
+  },
+  toCollect(){
+    this.setData({
+      ifCollect: !this.data.ifCollect
+    })
+  },
+  showShareRule(){
+    this.setData({
+      ifShareRule: !this.data.ifShareRule
+    })
+  },
+  cloeseShareRule(){
+    this.setData({
+      ifShareRule: !this.data.ifShareRule
+    })
+  },
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  cancel(){
+    this.setData({
+      ifOutput: false
+    })
+  },
+  showOutput(){
+    this.setData({
+      ifOutput: true
+    })
+  },
+  makeCall(){
+    wx.makePhoneCall({
+      phoneNumber: '1340000' // 仅为示例，并非真实的电话号码
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
