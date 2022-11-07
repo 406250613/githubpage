487d70927f5bb9e47974f4800bf66d96da6dcea8
JiangWenhong <33144275+406250613@users.noreply.github.com>
2022-11-07 15:01:00 +0800

    fix

--- 918/pwa-sw.js
+++ 918/pwa-sw.js
@@ -1,7 +1,12 @@
+
 self.addEventListener('install', function(e) {
     console.log('[Service Worker] Install');
+    // await cache.addAll(contentToCache);
 });
 
 self.addEventListener('fetch', function(e) {
     console.log('[Service Worker] Fetched resource '+e.request.url);
+    fetch(e.request).then((response) => {
+        console.log('[Service Worker] response: ' + response)
+    })
 });
